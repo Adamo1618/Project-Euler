c61f0e0a61173d9757786edb304d8eae9444a821
Adamo1618 <adamekeroth@hotmail.com>
2018-01-28 15:08:47 +0100

    Update Problem7.java

--- Problem7.java
+++ Problem7.java
@@ -1,6 +1,7 @@
 package euler;
 
 import java.util.ArrayList;
+import euler.Primelist;
 
 public class Problem7 {
 
@@ -8,36 +9,14 @@ public class Problem7 {
 		// TODO Auto-generated method stub
 		//Solved
 		int i = 2;
-		int num = 10001;
+		int num = 6;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
-		while(list.size() < num)
+		while(list.size() <= num)
 		{
-			list = extendPrimeList(list, i);
+			list = Primelist.addPrimeToList(list, i);
 			i++;
 		}
-		System.out.print(list.get(num-1));
-
-	}
-	public static ArrayList<Integer> extendPrimeList(ArrayList<Integer> list, int number)
-	{
-		
-		int i = 0;
-		boolean isprime = true;
-		while(i < list.size() && i < Math.sqrt(number))
-		{
-			if(number % list.get(i) == 0)
-			{
-				isprime = false;
-				break;
-			}
-			i++;
-		}
-		if(isprime)
-		{
-			list.add(number);
-		}
-		
-		return list;
+		System.out.print(list.get(num));
 	}
 }
